2f7dffa04329b53781a2b9740369ef3ea33b2c72
nir <nrbnlulu@gmail.com>
2025-03-18 13:37:05 +0200

    Update warning message formatting in diffcov plugin

--- src/cibot/plugins/diffcov.py
+++ src/cibot/plugins/diffcov.py
@@ -92,7 +92,7 @@ class DiffCovPlugin(CiBotPlugin):
 				self.backend.create_pr_review_comment(
 					PrReviewComment(
 						content=f"⛔ Missing coverage from line {start_line} to line {end_line}" + 
-						"\n<sub><sup>*Don't comment here, it will be deleted*</sup></sub>",
+						"\n<sub><sup>**Don't comment here, it will be deleted**</sup></sub>",
 						content_id=DIFF_COV_REVIEW_COMMENT_ID,
 						start_line=start_line if end_line != start_line else None,
 						end_line=end_line or start_line,
